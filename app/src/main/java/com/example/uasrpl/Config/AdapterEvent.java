43877a0ca7df5660313caefee9b164013e7895fd
Brielt Bella Gracitwo <44756651+EGraci@users.noreply.github.com>
2022-06-12 01:11:51 +0700

    komen data seminar

--- app/src/main/java/com/example/uasrpl/Config/AdapterEvent.java
+++ app/src/main/java/com/example/uasrpl/Config/AdapterEvent.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -35,10 +37,18 @@ public class AdapterEvent extends RecyclerView.Adapter<AdapterEvent.EventSet>{
     @Override
     public void onBindViewHolder(@NonNull EventSet holder, int position) {
         Glide.with(ct).load(ls_event.get(position).getGambar()).into(holder.imageView);
-        holder.view.setOnClickListener(new View.OnClickListener() {
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("clik","berhasil");
+                // pindah ke class sampean mau
+                // ls_event.get(position).getIdEvent() ==> data id event
+                // ls_event.get(position).getGambar() ==> data gambar
+                // ls_event.get(position).getJudul() ==> data judul seminar
+                // ls_event.get(position).getDeskripsi() ==> data deskripsi seminar
+                // ls_event.get(position).getPembicara() ==> data pembicara seminar
+                // ls_event.get(position).getJamMulai() ==> data jam mulai
+                // ls_event.get(position).getJamSelesai() ==> data jam selesai
+                Toast.makeText(holder.view.getContext(), "ini untuk pindah class baru", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -50,11 +60,14 @@ public class AdapterEvent extends RecyclerView.Adapter<AdapterEvent.EventSet>{
 
     public class EventSet extends RecyclerView.ViewHolder{
         ImageView imageView;
+        CardView cardView;
         View view;
         public EventSet(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.imageEvent);
+            cardView = itemView.findViewById(R.id.row_konten);
             view = itemView;
         }
+
     }
 }
